e6f27542aaa6c6b8a74d6e93839059ec139f7579
kom73509 <iskom45@yahoo.fr>
2017-11-27 18:10:19 +0100

    changement fenetre-main-panel

---
+++ wargame/PanneauCarte.java
@@ -0,0 +1,24 @@
+package wargame;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class PanneauCarte extends JPanel implements IConfig {
+	
+	    private Carte map ;		// la carte à dessiner
+
+	    public PanneauCarte(Carte carte)
+	    {
+	    	setSize(new Dimension(IConfig.LARGEUR_CARTE * IConfig.NB_PIX_CASE, IConfig.HAUTEUR_CARTE * IConfig.NB_PIX_CASE ));
+		map = carte;
+	    }
+	    
+	  public void paintComponent(Graphics g)
+	  {
+	      map.toutDessiner(g);
+	  }
+	  public Carte getMap(){ return map; }
+
+}
